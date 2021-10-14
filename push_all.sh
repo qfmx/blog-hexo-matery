547136c358e55f2ec05659ddff2c6ac09d6d0d4a
allows <allows@e.com>
2021-10-14 15:14:23 +0800

    update

--- push_all.sh
+++ push_all.sh
@@ -1,5 +1,7 @@
-echo "start push github";
-git add -A;
-git commit -am "update";
-git push;
-echo "end push github";
+#!/bin/sh
+
+echo "start push github"
+git add -A
+git commit -am "update"
+git push
+echo "end push github"
